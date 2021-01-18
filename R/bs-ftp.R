8ba0dcac972ce246ce14c1a25875b4c843fa5475
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-01-18 12:42:19 -0400

    add ftp tools

---
+++ R/bs-ftp.R
@@ -0,0 +1,131 @@
+
+#' Cache, fetch, and list files from the BSRTO FTP server
+#'
+#' @param x A path relative to /pub/dfo
+#' @param ftp The location of the FTP server
+#' @param cache Path to a local copy of /pub/dfo where files will be cached
+#'   or written.
+#' @param quiet Use `TRUE` to suppress progress messages
+#' @param recursive Use `TRUE` to recurse directories
+#' @param pattern A regular expression against which files are matched
+#'   or `NULL` to match all files.
+#' @param print Use `TRUE` to print out matching files as the listings are
+#'   downloaded.
+#'
+#' @return `bs_ftp_cached()` returns the file path to the local version of the
+#'   file after downloading; `bs_ftp_list()` returns a list of files (not
+#'   directories!) that match `pattern`.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' bs_ftp_cached("barrow/BarrowStraitDataSummary.xlsx", cache = tempfile())
+#' bs_ftp_list("barrow")
+#' }
+bs_ftp_cached <- function(x,
+                          ftp = getOption("bsrto.ftp", "ftp://dfoftp.ocean.dal.ca/pub/dfo"),
+                          cache = getOption("bsrto.cache", file.path(getwd(), "bsrto_cache")),
+                          quiet = FALSE) {
+  if (length(x) != 1) {
+    results <-  vapply(
+      x,
+      bs_ftp_cached,
+      ftp = ftp,
+      cache = cache,
+      quiet = quiet,
+      FUN.VALUE = character(1)
+    )
+
+    return(unname(results))
+  }
+
+  ftp <- gsub("/?$", "/", ftp)
+  cached_path <- file.path(cache, x)
+
+  if (!file.exists(cached_path)) {
+    cached_dir <- dirname(cached_path)
+    if (!dir.exists(cached_dir)) dir.create(cached_dir, recursive = TRUE)
+    ftp_address <- paste0(ftp, x)
+    if (!quiet) message(glue("Downloading '{ ftp_address }' => '{ cached_path }'"))
+    curl::curl_download(ftp_address, cached_path)
+  }
+
+  cached_path
+}
+
+#' @rdname bs_ftp_cached
+#' @export
+bs_ftp_list <- function(x, pattern = NULL, recursive = FALSE,
+                        ftp = getOption("bsrto.ftp", "ftp://dfoftp.ocean.dal.ca/pub/dfo"),
+                        print = FALSE, quiet = FALSE) {
+  if (length(x) != 1) {
+    results <- lapply(
+      x,
+      bs_ftp_list,
+      pattern = pattern,
+      recursive = recursive,
+      ftp = ftp,
+      quiet = quiet,
+      print = print
+    )
+
+    return(as.character(unlist(results)))
+  }
+
+  # make sure x and ftp end with a trailing slash
+  if (!(x %in% c("", "/"))) {
+    x <- gsub("/?$", "/", x)
+  }
+  ftp <- gsub("/?$", "/", ftp)
+
+  ftp_address <- paste0(ftp, x)
+  if (!quiet) message(glue("Listing directory '{ ftp_address }'"))
+  result <- curl::curl_fetch_memory(ftp_address)
+
+  # parse the results
+  con <- textConnection(rawToChar(result$content), encoding = "UTF-8")
+  on.exit(close(con))
+  items <- readLines(con)
+  items <- items[items != ""]
+
+  if (length(items) == 0) {
+    return(character(0))
+  }
+
+  # filenames can contain whitespace but fields can't (?)
+  # which makes parsing this complicated (could also use stringr::str_split())
+  name <- paste0(x, gsub("([A-Za-z0-9.-]+\\s+){8}", "", items))
+  is_dir <- substr(items, 1, 1) == "d"
+
+  files <- name[!is_dir]
+  dirs <- name[is_dir]
+
+  # filter by pattern
+  if (!is.null(pattern)) {
+    files <- files[grepl(pattern, basename(files))]
+  }
+
+  if (print) {
+    cat(paste0(files, collapse = "\n"))
+    cat("\n")
+  }
+
+  if (recursive && (length(dirs) > 0)) {
+    subdirectory_listings <- lapply(
+      dirs,
+      bs_ftp_list,
+      pattern = pattern,
+      recursive = TRUE,
+      quiet = quiet,
+      print = FALSE
+    )
+
+    files <- c(files, unlist(subdirectory_listings))
+  }
+
+  if (print) {
+    invisible(files)
+  } else {
+    files
+  }
+}
---
+++ tests/testthat/test-bs-ftp.R
@@ -0,0 +1,45 @@
+
+test_that("bs_ftp_list works", {
+  skip_if_offline()
+  skip_if_not(interactive())
+
+  expect_is(bs_ftp_list("barrow", quiet = TRUE), "character")
+  expect_message(bs_ftp_list("barrow", quiet = FALSE), "Listing directory")
+  expect_output(bs_ftp_list("barrow", print = TRUE, quiet = TRUE), "barrow/")
+
+  mctd_files <- bs_ftp_list("barrow/1998/mctd", recursive = TRUE, quiet = TRUE)
+  mctd_files2 <- bs_ftp_list(
+    c("barrow/1998/mctd",
+      "barrow/1998/mctd/processed",
+      "barrow/1998/mctd/raw",
+      "barrow/1998/mctd/raw/CalibrationFiles"
+    ),
+    quiet = TRUE
+  )
+  expect_identical(mctd_files, mctd_files2)
+
+  mctd_files_asc <- bs_ftp_list(
+    "barrow/1998/mctd", "\\.ASC$",
+    recursive = TRUE,
+    quiet = TRUE
+  )
+
+  expect_true(all(grepl("\\.ASC$", mctd_files_asc)))
+  expect_identical(bs_ftp_list(character()), character())
+})
+
+test_that("bs_ftp_cached() works", {
+  skip_if_offline()
+  skip_if_not(interactive())
+
+  expect_identical(bs_ftp_cached(character()), character())
+
+  cache <- tempfile()
+  expect_identical(
+    bs_ftp_cached("barrow/BarrowStraitDataSummary.xlsx", cache = cache, quiet = TRUE),
+    file.path(cache, "barrow/BarrowStraitDataSummary.xlsx")
+  )
+  expect_true(file.exists(file.path(cache, "barrow/BarrowStraitDataSummary.xlsx")))
+
+  unlink(cache, recursive = TRUE)
+})
