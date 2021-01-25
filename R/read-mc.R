94e5d542ed7b8a557897b0aba6ed248e1831ad27
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-01-25 11:12:34 -0400

    mci parsing test

--- R/read-mc.R
+++ R/read-mc.R
@@ -2,6 +2,11 @@
 #' Read mcA, mcI, and mcH files
 #'
 #' @inheritParams read_imm
+#' @inheritParams bs_progress
+#' @param col_names Column names, if guessed wrong by `guess_mc_col_names()`.
+#' @param skip Number of columns before table, if guessed wrong by
+#'   `guess_mc_skip()`.
+#'
 #' @param year The year to use to specify column names in the range
 #'   2017-2019.
 #'
@@ -13,7 +18,11 @@
 #' read_mc(mca_file)
 #' read_mc_vector(mca_file)
 #'
-read_mc <- function(file, col_names = guess_mc_col_names(file),
+#' mci_file <- bs_example("mcI/19101023.mcI")
+#' read_mci_meta(mci_file)
+#' read_mci_meta_vector(mci_file)
+#'
+read_mc <- function(file, col_names = guess_mc_col_names(file, skip),
                     skip = guess_mc_skip(file), tz = "UTC") {
   stopifnot(length(file) == 1)
   read_mc_vector(file, col_names = col_names, skip = skip, tz = tz)[-1]
@@ -22,7 +31,7 @@ read_mc <- function(file, col_names = guess_mc_col_names(file),
 #' @rdname read_mc
 #' @export
 read_mc_vector <- function(file_vector,
-                           col_names = guess_mc_col_names(file_vector[1]),
+                           col_names = guess_mc_col_names(file_vector[1], skip),
                            skip = guess_mc_skip(file_vector[1]),
                            tz = "UTC") {
   pb <- bs_progress(file_vector)
@@ -90,19 +99,23 @@ guess_mc_skip <- function(file) {
   switch(
     tolower(tools::file_ext(file)),
     "NA" = NULL,
-    "mci" = 3,
+    "mci" = {
+      first_line <- readr::read_lines(file, n_max = 1)
+      n_equals <- stringr::str_count(first_line, "=")[1]
+      if (identical(n_equals, 1L)) 3 else 0
+    },
     0
   )
 }
 
 #' @rdname read_mc
 #' @export
-guess_mc_col_names <- function(file) {
+guess_mc_col_names <- function(file, skip = guess_mc_skip(file)) {
   switch(
     tolower(tools::file_ext(file)),
     "NA" = NULL,
     "mca" = {
-      first_line <- readr::read_lines(file, n_max = 1)
+      first_line <- readr::read_lines(file, skip = skip, n_max = 1)
       n_commas <- stringr::str_count(first_line, ",")[1]
       switch(
         as.character(n_commas),
@@ -113,8 +126,8 @@ guess_mc_col_names <- function(file) {
       )
     },
     "mci" = {
-      first_line <- readr::read_lines(file, n_max = 4)
-      n_commas <- stringr::str_count(first_line, ",")[4]
+      first_line <- readr::read_lines(file, skip = skip, n_max = 1)
+      n_commas <- stringr::str_count(first_line, ",")[1]
       switch(
         as.character(n_commas),
         "6" = mci_col_names("2017"),
@@ -165,5 +178,51 @@ mci_col_names <- function(year) {
       "date", "time"
     ),
     abort(glue("Can't fetch mcI column names for year '{ year }'"))
+  )
+}
+
+#' @rdname read_mc
+#' @export
+read_mci_meta <- function(file, tz = "UTC") {
+  stopifnot(length(file) == 1)
+  read_mci_meta_vector(file, tz = tz)[-1]
+}
+
+#' @rdname read_mc
+#' @export
+read_mci_meta_vector <- function(file_vector, tz = "UTC") {
+  pb <- bs_progress(file_vector)
+  on.exit(bs_progress_finish(pb))
+
+  results <- lapply(file_vector, read_mci_meta_single, pb = pb)
+
+  results_all <- vctrs::vec_rbind(
+    tibble::tibble(
+      `start sample number` = character(),
+      `start time` = character()
+    ),
+    !!! results
+  )
+
+  results_all$`start time` <- readr::parse_datetime(
+    results_all$`start time`,
+    format = "%d %b %Y %H:%M:%S",
+    locale = readr::locale(tz = tz)
+  )
+
+  vctrs::vec_cbind(
+    file = file_vector,
+    results_all
+  )
+}
+
+read_mci_meta_single <- function(file, pb = NULL) {
+  bs_tick(pb, file)
+
+  lines <- stringr::str_trim(readr::read_lines(file, n_max = 3))
+  value_key <- stringr::str_split_fixed(lines[lines != ""], "\\s*=\\s*", n = 2)
+  tibble::new_tibble(
+    stats::setNames(as.list(value_key[, 2]), value_key[, 1]),
+    nrow = 1
   )
 }
--- tests/testthat/test-read-mc.R
+++ tests/testthat/test-read-mc.R
@@ -39,6 +39,26 @@ test_that("read_mc() works for mcI files", {
     read_mc_vector(mc_file)[-1]
   )
   expect_true("date_time" %in% colnames(read_mc_vector(character())))
+
+  # malformed file should not error
+  expect_warning(
+    read_mc(bs_example("mcI/20010323.mcI"), col_names = mci_col_names(2018)),
+    "parsing failure"
+  )
+})
+
+test_that("read_mci_meta() works", {
+  mc_file <- bs_example("mcI/19101023.mcI")
+  mc <- read_mci_meta(mc_file)
+  expect_identical(
+    mc$`start time`[1],
+    readr::parse_datetime("2019-10-10 00:00:10", locale = readr::locale(tz = "UTC"))
+  )
+  expect_identical(
+    read_mci_meta(mc_file),
+    read_mci_meta_vector(mc_file)[-1]
+  )
+  expect_true("start time" %in% colnames(read_mci_meta_vector(character())))
 })
 
 test_that("read_mc() works for all mcA files in the cache", {
@@ -53,3 +73,32 @@ test_that("read_mc() works for all mcA files in the cache", {
   expect_true(all(all$temperature > -2 & all$temperature < 5, na.rm = TRUE))
 })
 
+test_that("read_mc() works for all mcI files in the cache", {
+  skip_if_not(bs_has_cache())
+
+  # >1,300 files
+  files <- bs_ftp_snapshot_latest$file[grepl("\\.mcI$", bs_ftp_snapshot_latest$file)]
+  cached <- bs_cache(files)
+  exists <- file.exists(cached)
+  files <- files[exists]
+  cached <- cached[exists]
+
+  # different directories have slightly different mcI file structures
+  no_header_dirs <- c(
+    "barrow/2017/bsrto/node/IPS/data/sbe",
+    "barrow/2017/bsrto/real-time/mcI"
+  )
+  mci_no_header <- cached[dirname(files) %in% no_header_dirs]
+  no_header <- read_mc_vector(mci_no_header)
+  expect_true(all(no_header$temperature > -2 & no_header$temperature < 6, na.rm = TRUE))
+
+  with_header <- setdiff(cached, mci_no_header)
+  # this file is tested above
+  with_header <- with_header[basename(with_header) != "20010323.mcI"]
+
+  all <- read_mc_vector(with_header)
+  expect_true(all(all$temperature > -2 & all$temperature < 5, na.rm = TRUE))
+
+  all_meta <- read_mci_meta_vector(with_header)
+  expect_true(all(!is.na(all_meta$`start time`[file.size(with_header) > 0])))
+})
