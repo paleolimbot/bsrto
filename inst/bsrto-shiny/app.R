4a8b3f0e5eba221fd8c88c2b0f2006aeea7e5638
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-03-18 17:12:10 -0300

    working bilingual shiny app layout

--- inst/bsrto-shiny/app.R
+++ inst/bsrto-shiny/app.R
@@ -1,154 +1,103 @@
 
-library(dplyr)
-library(ggplot2)
 library(shiny)
 
-theme_set(theme_bw())
+# Setting the default language to fr because there is auto-detection of
+# language based on the browser. This allows this to be bested since
+# development is taking place in english
+i18n <- shiny.i18n::Translator$new(translation_json_path = "translation.json")
+i18n$set_translation_language("fr")
 
-# Data -----------------
 
-# eventually these should be SQLite tables or something else
-# that can be lazily queried
-ctd <- readr::read_csv(
-    here::here("ctd.csv"),
-    col_types = readr::cols(
-        file = readr::col_character(),
-        date_time = readr::col_datetime(),
-        .default = readr::col_double()
+navbarPageWithInputs <- function(..., inputs) {
+  navbar <- navbarPage(...)
+  form <- tags$form(class = "navbar-form", inputs)
+  navbar[[3]][[1]]$children[[1]] <- htmltools::tagAppendChild(
+    navbar[[3]][[1]]$children[[1]], form)
+  navbar
+}
+
+ui <- tags$div(
+  shinyjs::useShinyjs(),
+  shiny.i18n::usei18n(i18n),
+  navbarPageWithInputs(
+    i18n$t("bsrto_full"),
+    navbarMenu(
+      "Data",
+      tabPanel(i18n$t("Water Properties")),
+      tabPanel(i18n$t("Currents")),
+      tabPanel(i18n$t("Shore-Station Barometer")),
+      tabPanel(i18n$t("Sound")),
+      tabPanel(i18n$t("Ice Thickness")),
+      tabPanel(i18n$t("Resolute Weather"))
+    ),
+    tabPanel(i18n$t("Logs")),
+    tabPanel(i18n$t("About")),
+    inputs = tags$div(
+      style = "float: right;",
+      actionButton("lang_toggle", label = i18n$t("other_lang")),
+      verbatimTextOutput("lang_dummy"),
+      tags$script(HTML('Shiny.addCustomMessageHandler("changetitle", function(x) {document.title=x});'))
     )
+  )
 )
 
-# Utility --------------
+server <- function(input, output, session) {
 
-plot_ctd <- function(data, var, lab = var,
-                     datetime_range = range(data$date_time, na.rm = TRUE),
-                     reverse = FALSE, mooring_depth_guide = FALSE) {
-    ggplot(data, aes(date_time, .data[[var]])) +
-        geom_point(aes(col = factor(depth_label, levels = c("40", "60", "160")))) +
-        scale_x_datetime(limits = datetime_range) +
-        (if (reverse) scale_y_reverse()) +
-        scale_color_brewer(
-            type = "qual", palette = 1,
-            limits = factor(c(40, 60, 160)),
-            labels = paste(c(40, 60, 160), "m"),
-            name = "Mooring Depth",
-            guide = if (!mooring_depth_guide) "none" else "legend"
-        ) +
-        labs(x = NULL, y = lab, col = "Mooring Depth") +
-        theme(legend.position = "top")
-}
+  # Fancy way to get the browser's idea of what the language is
+  # this won't set the input value right away; code needs to treat this like
+  # an input value that could be set by the user at any time.
+  shinyjs::runjs("
+    var usr_lang_initial_auto =  window.navigator.userLanguage || window.navigator.language;
+    Shiny.setInputValue('lang_initial_auto', usr_lang_initial_auto);
+  ")
 
-if (FALSE) {
-    plot_ctd(ctd, "temperature", "Temperature [°C]", mooring_depth_guide = TRUE)
-    plot_ctd(ctd, "conductivity", "Conductivity [S/m]")
-    plot_ctd(ctd, "pressure", "Pressure [dbar]", reverse = TRUE)
-    plot_ctd(ctd, "oxygen", "Oxygen [mg/L]")
-    plot_ctd(ctd, "salinity_calc", "Salinity [psal]")
-    plot_ctd(ctd, "sound_speed_calc", "Sound Speed [m/s]")
-}
+  # An empty output that is rendered initially and when 'lang_initial_auto'
+  # is changed (on page load)
+  output$lang_dummy <- renderText({
+    query <- parseQueryString(session$clientData$url_search)
+    new_lang <- NULL
+    has_initial_lang <- exists("lang_initial", session$userData)
 
-# Globals ---------------
+    if (!has_initial_lang && !is.null(query$lang)) {
+      new_lang <- query$lang
+      session$userData$lang_initial <- query$lang
+    } else if (!has_initial_lang && !is.null(input$lang_initial_auto)) {
+      if (is.character(input$lang_initial_auto) && startsWith(input$lang_initial_auto, "fr")) {
+        new_lang <- "fr"
+      } else {
+        new_lang <- "en"
+      }
+      session$userData$lang_initial <- input$initial_lang_auto
+    } else if (!exists("lang", session$userData)) {
+      new_lang <- "fr"
+    }
 
-globals <- list(
-    first_date = as.Date(min(ctd$date_time)),
-    last_date = as.Date(max(ctd$date_time))
-)
+    if (!is.null(new_lang)) {
+      session$userData$lang <- new_lang
+      shiny.i18n::update_lang(session, new_lang)
+      updateQueryString(paste0("?lang=", new_lang), mode = "replace")
 
+      # window title doesn't quite work with i18n$t()
+      session$sendCustomMessage(
+        "changetitle",
+        i18n$get_translations()["window_title", new_lang]
+      )
+    }
+  })
 
-# UI -----------------
+  # Observe click of the "Francais" or "English" button and toggle the language
+  observeEvent(input$lang_toggle, {
+    new_lang <- if (session$userData$lang == "en") "fr" else "en"
+    shiny.i18n::update_lang(session, new_lang)
+    updateQueryString(paste0("?lang=", new_lang), mode = "replace")
+    session$userData$lang <- new_lang
 
-ui <- fluidPage(
-
-    sidebarLayout(
-        sidebarPanel(
-            dateRangeInput("date_range", "Date Range",
-                           start = globals$last_date - 7L,
-                           end = globals$last_date + 1L,
-                           min = globals$first_date,
-                           max = globals$last_date + 1L),
-        ),
-
-        mainPanel(
-           plotOutput("ctd_temperature", height = 190),
-           plotOutput("ctd_conductivity", height = 150),
-           plotOutput("ctd_pressure", height = 150),
-           plotOutput("ctd_oxygen", height = 150),
-           plotOutput("ctd_salinity", height = 150),
-           plotOutput("ctd_sound_speed", height = 150)
-        )
+    # window title doesn't quite work with i18n$t()
+    session$sendCustomMessage(
+      "changetitle",
+      i18n$get_translations()["window_title", new_lang]
     )
-)
-
-
-# Server ----------------
-
-server <- function(input, output) {
-
-    datetime_range <- reactive({
-        dt_range <- as.POSIXct(input$date_range)
-        attr(dt_range, "tzone") <- "UTC"
-        dt_range
-    })
-
-    ctd_filter <- reactive({
-        dt_range <- datetime_range()
-        ctd %>%
-            filter(
-                date_time >= !! dt_range[1],
-                date_time < !! dt_range[2]
-            )
-    })
-
-    output$ctd_temperature <- renderPlot({
-        plot_ctd(
-            ctd_filter(),
-            "temperature", "Temperature [°C]",
-            datetime_range = datetime_range(),
-            mooring_depth_guide = TRUE
-        )
-    })
-
-    output$ctd_conductivity <- renderPlot({
-        plot_ctd(
-            ctd_filter(),
-            "conductivity", "Conductivity [S/m]",
-            datetime_range = datetime_range()
-        )
-    })
-
-    output$ctd_pressure <- renderPlot({
-        plot_ctd(
-            ctd_filter(),
-            "conductivity", "Conductivity [S/m]",
-            datetime_range = datetime_range()
-        )
-    })
-
-    output$ctd_oxygen <- renderPlot({
-        plot_ctd(
-            ctd_filter(),
-            "oxygen", "Dissolved oxygen [mg/L]",
-            datetime_range = datetime_range()
-        )
-    })
-
-    output$ctd_salinity <- renderPlot({
-        plot_ctd(
-            ctd_filter(),
-            "salinity_calc", "Salinity [psal]",
-            datetime_range = datetime_range()
-        )
-    })
-
-    output$ctd_sound_speed <- renderPlot({
-        plot_ctd(
-            ctd_filter(),
-            "sound_speed_calc", "Sound speed [m/s]",
-            datetime_range = datetime_range()
-        )
-    })
+  })
 }
 
-# Run ------------
-
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
